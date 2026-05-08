542ad8dc35774868cc87613b1a431683bda1438c
Athulraj K <65604069+athul183@users.noreply.github.com>
2026-05-08 16:09:09 +0530

    Update database.py

--- database.py
+++ database.py
@@ -1,1 +1,11 @@
-fake_users_db = {}
+import os
+from motor.motor_asyncio import AsyncIOMotorClient
+from dotenv import load_dotenv
+
+load_dotenv()
+
+MONGODB_URL = os.getenv("MONGODB_URL", "mongodb://localhost:27017")
+
+client = AsyncIOMotorClient(MONGODB_URL)
+db = client.devflow_db
+users_collection = db.users
